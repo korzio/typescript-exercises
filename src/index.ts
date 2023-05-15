59a31f73a3e509f83798d31e140f2411714f16dc
Alex Korzhikov <korzio@gmail.com>
2023-05-15 18:25:15 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+// ⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇
+//   Exercises
+// ⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈
+
+// To keep the console relatively clean, you may
+// comment previous exercises as you progress.
+
+// Now, ready to do you some TypeScript?!
+
+// ======== Exercise 1: Basic types ========
+import exercise1 from './Exercise_1_Basic_Types/exercise1';
+exercise1();
+
+// ======== Exercise 2: Interfaces ==========
+// import exercise2 from './Exercise_2_Interfaces/exercise2';
+// exercise2();
+
+// ======== Exercise 3: Functions ============
+// import exercise3 from './Exercise_3_Functions/exercise3';
+// exercise3();
+
+// ======== Exercise 4: Types =========
+// import exercise4 from './Exercise_4_Types/exercise4';
+// exercise4();
+
+// ======== Exercise 5: Code flow  =========
+// import exercise5 from './Exercise_5_Classes/exercise5';
+// exercise5();
